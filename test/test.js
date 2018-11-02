82b368d3db69c193eb369331c7bfc9310b8e0df4
Alex Buck <buckatech@gmail.com>
2018-11-02 01:54:09 -0600

    sweet integration victory

--- test/test.js
+++ test/test.js
@@ -2,35 +2,43 @@ const chai = require('chai');
 const should = chai.should();
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
-const nock = require('nock');
 
 const app = require('../express_server.js');
 const indexController = require('../controllers/indexController');
 const index = require('../routes/index');
 
-indexController.testVar = 'hi'
-console.log(indexController)
 chai.use(chaiHttp);
 
-const agent = chai.request.agent(app);
 
 describe('Integration test with session active', () => {
-  // it('should return the register page with text', () => {
-  //   return chai.request(app)
-  //       .get('/register')
-  //       .then((response) => {
-  //         response.should.have.status(200);
-  //         response.should.be.html;
-  //       })
-  //       .catch((error) => {
-  //         throw error;
-  //       });
-  // });
   it('should return the home page with a cookie', () => {
+    indexController.testVar = 'hi';
     return chai.request(app)
         .get('/')
         .then((response) => {
-          console.log(response.text)
+          const string = response.text;
+          const matchReg = string.match(/<p>([\s\S]*)?<\/p>/i)||[];
+          const out = matchReg[1] || '';
+          expect(out).to.be.length(6);
+          expect(out).to.be.a('string');
+          response.should.have.status(200);
+          response.should.be.html;
+          delete indexController.testVar;
+        })
+        .catch((error) => {
+          throw error;
+        });
+  });
+});
+describe('Integration test with no session', () => {
+  it('should return the home page with no cookie', () => {
+    return chai.request(app)
+        .get('/')
+        .then((response) => {
+          const string = response.text;
+          const matchReg = string.match(/<p>([\s\S]*)?<\/p>/i)||[];
+          const out = matchReg[1] || '';
+          expect(out).to.not.be.length(6);
           response.should.have.status(200);
           response.should.be.html;
         })
@@ -38,55 +46,5 @@ describe('Integration test with session active', () => {
           throw error;
         });
   });
-  
-  // it('home should check cookie', () => {
-  //   return chai.request.agent(app)
-  //       .post('/register')
-  //       .send({email: 'email@email.com', password: 'pass@pass.com'})
-  //       .then((res) => {
-  //         const str = res.text;
-  //         const m = str.match(/<p>([\s\S]*)?<\/p>/i)||[];
-  //         const out = m[1] || '';
-  //         res.cookie = out
-  //         const parentres = res.cookie
-  //         expect(out).to.be.length(6);
-  //         expect(out).to.be.a('string');
-  //         //console.log(res.cookie)
-  //         return agent.get('/')
-  //             .then((res) => {
-  //               nock('/')
-  //                 .get('/')
-  //                 .reply(200, {
-  //                   cookie: parentres
-  //                 })
-  //                 console.log(res)
-  //             });
-  //       });
-  // });
-
-  // it('should post the user to database', () => {
-  //   return chai.request(app)
-  //       .post('/register')
-  //       .send({email: 'email@email.com', password: 'pass@pass.com'})
-  //       .then((response) => {
-  //         response.should.have.status(200);
-  //         response.should.be.html;
-  //       })
-  //       .catch((error) => {
-  //         throw error;
-  //       });
-  // });
-
-  // it('should return a 404 for a route that does not exist', () => {
-  //   return chai.request(app)
-  //       .get('/sad')
-  //       .then((response) => {
-  //         response.should.have.status(404);
-  //       })
-  //       .catch((error) => {
-  //         throw error;
-  //       });
-  // });
 });
-
 
