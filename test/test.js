6e5ae852f03bafba0be7a6ff2addacf569627eda
Alex Buck <buckatech@gmail.com>
2018-11-02 08:57:11 -0600

    added route tests for index get requests

--- test/test.js
+++ test/test.js
@@ -8,7 +8,78 @@ const indexController = require('../controllers/indexController');
 const index = require('../routes/index');
 
 chai.use(chaiHttp);
-
+describe('Index Route Tests', () => {
+  it('should return 200 and render text when / is called', () => {
+    return chai.request(app)
+        .get('/')
+        .then((response) => {
+          response.should.have.status(200);
+          response.should.be.html;
+        })
+        .catch((error) => {
+          throw error;
+        });
+  });
+  it('should return 200 and render JSON when /json is called', () => {
+    return chai.request(app)
+        .get('/json')
+        .then((response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+        })
+        .catch((error) => {
+          throw error;
+        });
+  });
+  it('should return 302 and redirect to youtube when /u/b2xV2n is called', () => {
+    return chai.request(app)
+        .get('/u/b2xV2n')
+        .then((response) => {
+          response.should.have.status(302);
+          response.should.be.text;
+          expect(response).to.redirect;
+          expect(response).to.redirectTo('https://youtube.com');
+        })
+        .catch((error) => {
+          throw error;
+        });
+  });
+  it('should return 302 and redirect to google when /u/h1h1h1 is called', () => {
+    return chai.request(app)
+        .get('/u/h1h1h1')
+        .then((response) => {
+          response.should.have.status(302);
+          response.should.be.text;
+          expect(response).to.redirect;
+          expect(response).to.redirectTo('https://google.ca');
+        })
+        .catch((error) => {
+          throw error;
+        });
+  });
+  it('should return 200 and render text when /login is called', () => {
+    return chai.request(app)
+        .get('/login')
+        .then((response) => {
+          response.should.have.status(200);
+          response.should.be.html;
+        })
+        .catch((error) => {
+          throw error;
+        });
+  });
+  it('should return 200 and render text when /register is called', () => {
+    return chai.request(app)
+        .get('/register')
+        .then((response) => {
+          response.should.have.status(200);
+          response.should.be.html;
+        })
+        .catch((error) => {
+          throw error;
+        });
+  });
+});
 
 describe('Integration test with session active', () => {
   it('should return the home page with a cookie', () => {
