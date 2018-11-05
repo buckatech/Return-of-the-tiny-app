7b85d888c88a0136901abd1b51e23174676e4f59
Alex Buck <buckatech@gmail.com>
2018-11-05 11:24:56 -0700

    added some basic css

--- routes/index.js
+++ routes/index.js
@@ -17,14 +17,17 @@ router.get('/json', indexController.render_JSON);
 router.get('/u/:shortURL', indexController.render_id);
 // Get login page
 router.get('/login', indexController.render_login);
-// Post to login
-router.post('/login', indexController.post_login);
 // Get register
 router.get('/register', indexController.render_register);
+// Post to login
+router.post('/login', indexController.post_login);
 // Post to register
 router.post('/register', indexController.post_register);
 // Post to logout
 router.post('/logout', indexController.post_logout);
+
+/* ERROR HANDLERS */
+
 // Get error handler loginerr
 router.get('/loginErr', indexController.render_loginErr);
 // Get error handler badowner
